af55ee2fbe418f16fecf86c88d977f733b5f5847
Bahari Bastian <baharibastian@Baharis-MacBook-Pro.local>
2018-10-12 13:44:15 +0700

    fix auto increment error

--- server/model/merchant.go
+++ server/model/merchant.go
@@ -1,14 +1,14 @@
-package model 
+package model
 
 type Merchant struct {
-	Merchant_id int `gorm:"AUTO_INCREMENT"`
-	Merchant_code string `gorm:"size:50"`
-	Merchant_name string `gorm:"size:100"`
+	Id                   int    `gorm:"AUTO_INCREMENT"`
+	Merchant_code        string `gorm:"size:50"`
+	Merchant_name        string `gorm:"size:100"`
 	Merchant_description string `gorm:"size:255"`
-	Merchant_status int `gorm:"size:11"`
-	Merchant_open string `gorm:"size:11"`
-	Merchant_close string `gorm:"size:11"`
-	Created_at string `gorm:"size:15"`
-	Updated_at string `gorm:"size:15"`
-	Deleted_at string `gorm:"size:15"`
+	Merchant_status      int    `gorm:"size:11"`
+	Merchant_open        string `gorm:"size:11"`
+	Merchant_close       string `gorm:"size:11"`
+	Created_at           string `gorm:"size:15"`
+	Updated_at           string `gorm:"size:15"`
+	Deleted_at           string `gorm:"size:15"`
 }
--- server/model/order.go
+++ server/model/order.go
@@ -3,18 +3,18 @@ package model
 // import (
 // 	"github.com/jinzhu/gorm"
 // )
-	
 
 type Order struct {
 	// gorm.Model
-	Order_id string	`gorm:"size:50"`
-	Merchant_id string `gorm:"size:50"`
-	User_id int `gorm:"size:11"`
-	Order_date string `gorm:"size:11"`
-	Order_total int `gorm:"size:11"`
-	Order_return int `gorm:"size:11"`
-	Order_status int `gorm:"size:11"`
-	Created_at string `gorm:"size:15"`
-	Updated_at string `gorm:"size:15"`
-	Deleted_at string `gorm:"size:15"`
+	Id           int    `gorm:"AUTO_INCREMENT"`
+	Order_id     string `gorm:"size:50"`
+	Merchant_id  string `gorm:"size:50"`
+	User_id      int    `gorm:"size:11"`
+	Order_date   string `gorm:"size:11"`
+	Order_total  int    `gorm:"size:11"`
+	Order_return int    `gorm:"size:11"`
+	Order_status int    `gorm:"size:11"`
+	Created_at   string `gorm:"size:15"`
+	Updated_at   string `gorm:"size:15"`
+	Deleted_at   string `gorm:"size:15"`
 }
