71c4bd6fb80520bc087cdaa9249f698f982447e8
adhistria <adhisatria1@outlook.com>
2018-10-13 00:25:23 +0700

    restore merchant

--- server/model/merchant.go
+++ server/model/merchant.go
@@ -12,3 +12,54 @@ type Merchant struct {
 	Updated_at           string `gorm:"size:15"`
 	Deleted_at           string `gorm:"size:15"`
 }
+
+func (p *Merchant) AddMerchant(db *gorm.DB) bool {
+	db.Create(&p)
+	err := db.NewRecord(p) //if success, will return false otherwise true
+	return err
+}
+ func (p *Merchant) DeleteMerchant(db *gorm.DB) (map[string]interface{}, []error) {
+	err := db.First(&p)
+	message_success := map[string]interface{}{
+		"Status": "Delete Success",
+	}
+	f := map[string]interface{}{
+		"Merchant Code": p.Merchant_code,
+		"Merchant Name": p.Merchant_name,
+		"Message":       message_success,
+	}
+	if len(err.GetErrors()) > 0 {
+		return nil, err.GetErrors()
+	}
+	err = db.Delete(&p)
+	if len(err.GetErrors()) > 0 {
+		return nil, err.GetErrors()
+	}
+	return f, nil
+}
+ func (p *Merchant) UpdateMerchant(db *gorm.DB) []error {
+	db.First(&p)
+	err := db.Save(&p)
+	return err.GetErrors()
+}
+ func GetAllMerchants(db *gorm.DB) ([]Merchant, []error) {
+	var merchants []Merchant
+	errors := db.Find(&merchants).GetErrors()
+	if len(errors) > 0 {
+		return nil, errors
+	}
+	var new_merchants []Merchant
+	for _, merch := range merchants {
+		fmt.Println(merch)
+		var merchant Merchant
+		fmt.Println(merch.Id)
+		db.Find(&merch).Related(&merch.Products) //SELECT * FROM merchants JOIN products ON merchants.id = products.merchant_id WHERE merchant_id = merch.id
+		merchant = merch
+		new_merchants = append(new_merchants, merchant)
+	}
+	return new_merchants, errors
+}
+ func (p *Merchant) GetMerchant(db *gorm.DB) []error {
+	err := db.First(&p)
+	return err.GetErrors()
+ }
