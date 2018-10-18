7aa0deb245eab55aaf40f2574bb15a20006990e1
Bahari Bastian <baharibastian@Baharis-MacBook-Pro.local>
2018-10-18 22:52:25 +0700

    delete all unused files

--- chatbot/test.py
+++
@@ -1,7 +0,0 @@
-import json
-import requests
-req = requests.get('https://server-buychat.herokuapp.com/products')
-response = json.loads(req.text)
-print(response['data'])
-for product in response['data']:
-    print(product['Product_name'])
